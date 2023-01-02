f301cbf7347173a59b3331fa3ffecab2d9b4baf5
Ken-Miyawa <kenokwesh@gmail.com>
2023-01-02 07:56:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - A c program that prints a line to the standard error
- * Return: Always 0 (Success)
+ * main - A program that prints a line to the standard erro
+ * Return: 1 (Success)
  */
 int main(void)
 {
-fputc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return(1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
